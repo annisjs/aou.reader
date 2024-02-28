3c387ef507e048d79b87f14448e59404e68910b1
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-02-28 15:21:13 -0600

    Updating cadence pattern query

--- R/cadence_pattern_query.R
+++ R/cadence_pattern_query.R
@@ -25,6 +25,22 @@ cadence_pattern_query <- function(anchor_date_table=NULL,before=NULL,after=NULL)
 {
     dest <- "cadence_pattern_query_result.csv"
     query <- "
+        WITH sleep_table AS (
+            SELECT person_id, 
+                end_sleep, 
+                LEAD(start_sleep) OVER(PARTITION BY person_id ORDER BY start_sleep) AS next_sleep,
+            FROM
+            (
+                SELECT person_id,
+                    MIN(start_datetime) AS start_sleep,
+                    MAX(DATETIME_ADD(start_datetime, INTERVAL cast(duration_in_min*60 AS INT64) SECOND)) AS end_sleep,
+                FROM
+                    sleep_level
+                WHERE 
+                    is_main_sleep = 'true'
+                GROUP BY person_id, sleep_date
+            )
+        )
         SELECT * FROM 
         (
             SELECT person_id, cast(datetime AS DATE) as date, cadence, COUNT(cadence) as count
@@ -41,15 +57,17 @@ cadence_pattern_query <- function(anchor_date_table=NULL,before=NULL,after=NULL)
                     WHEN steps BETWEEN 100 AND 119 THEN 'brisk_walking'
                     ELSE 'all_faster_ambulation' END AS cadence,
                 FROM steps_intraday s
-                LEFT JOIN heart_rate_minute_level h ON (s.datetime = h.datetime AND s.person_id = h.person_id)
-                WHERE h.heart_rate_value > 0
+                INNER JOIN heart_rate_minute_level h ON (s.datetime = h.datetime AND s.person_id = h.person_id)
+                INNER JOIN sleep_table st ON (st.person_id = s.person_id AND 
+                                            s.datetime BETWEEN st.end_sleep AND st.next_sleep)
+                WHERE h.heart_rate_value > 0 
             )
             GROUP BY person_id, cadence, date
         )
         PIVOT (SUM(count) FOR cadence IN ('non_movement','incidental_movement',
         'sporadic_movement','purposeful_movement','slow_walking','medium_walking',
         'brisk_walking','all_faster_ambulation'))
-    "
+        "
     result_all <- download_big_data(query,dest)
     result_all <- window_data(result_all,"date",anchor_date_table,before,after)
     return(result_all)
