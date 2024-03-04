1e64cd1f3eb6601aed9a827cc9422042f17b47f9
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-03-04 10:13:00 -0600

    fixing typo in window_data function

--- R/window_data.R
+++ R/window_data.R
@@ -7,8 +7,8 @@ window_data <- function(dat,date_column,anchor_date_table,before,after)
         dat[,max_window_date := anchor_date + after]
         dat <- dat[get(date_column) >= min_window_date]
         dat <- dat[get(date_column) <= max_window_date]
-        dat[, min_window_dat := NULL]
-        dat[, max_window_dat := NULL]
+        dat[, min_window_date := NULL]
+        dat[, max_window_date := NULL]
         dat[, anchor_date := NULL]
     }
     return(dat)
