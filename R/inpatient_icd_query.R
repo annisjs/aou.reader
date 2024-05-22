6602d6e3052780aa3e945f1eba3dee2c3ef4ebcd
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-05-22 09:41:31 -0500

    Adding NULL condition for codes, which will produce all inpatient ICD codes

--- R/inpatient_icd_query.R
+++ R/inpatient_icd_query.R
@@ -23,29 +23,51 @@
 #' inpatient_dat <- inpatient_icd_query(c("I21","I21.%"))
 #' }
 #' @export 
-inpatient_icd_query <- function(codes,anchor_date_table=NULL,before=NULL,after=NULL)
+inpatient_icd_query <- function(codes=NULL,anchor_date_table=NULL,before=NULL,after=NULL)
 {
   dest <- "inpatient_icd_query_result.csv"
   code_clause <- paste('co.CONDITION_SOURCE_VALUE LIKE ',"'",codes,"'",collapse=' OR ',sep="")
-  query <- stringr::str_glue(
-    "SELECT co.person_id,co.condition_start_date,co.condition_source_value
-        FROM
-            `condition_occurrence` co
-            LEFT JOIN
-            `concept` c
-            ON (co.condition_source_concept_id = c.concept_id)
-            LEFT JOIN
-            `visit_occurrence` v
-            ON (co.visit_occurrence_id = v.visit_occurrence_id)
-        WHERE
-            c.vocabulary_id LIKE 'ICD%' AND
-            ({code_clause}) AND
-            (co.condition_type_concept_id IN (38000200,38000201,
-            38000202,38000203,38000204,38000205,38000214,
-            38000206,38000207,38000208,38000209,38000210,
-            38000211,38000212,38000213) OR
-            v.visit_concept_id IN (9201,9203))
-        ")
+  if (is.null(codes))
+  {
+    query <- stringr::str_glue(
+        "SELECT co.person_id,co.condition_start_date,co.condition_source_value
+            FROM
+                `condition_occurrence` co
+                LEFT JOIN
+                `concept` c
+                ON (co.condition_source_concept_id = c.concept_id)
+                LEFT JOIN
+                `visit_occurrence` v
+                ON (co.visit_occurrence_id = v.visit_occurrence_id)
+            WHERE
+                c.vocabulary_id LIKE 'ICD%' AND
+                (co.condition_type_concept_id IN (38000200,38000201,
+                38000202,38000203,38000204,38000205,38000214,
+                38000206,38000207,38000208,38000209,38000210,
+                38000211,38000212,38000213) OR
+                v.visit_concept_id IN (9201,9203))
+            ")
+  } else {
+    query <- stringr::str_glue(
+        "SELECT co.person_id,co.condition_start_date,co.condition_source_value
+            FROM
+                `condition_occurrence` co
+                LEFT JOIN
+                `concept` c
+                ON (co.condition_source_concept_id = c.concept_id)
+                LEFT JOIN
+                `visit_occurrence` v
+                ON (co.visit_occurrence_id = v.visit_occurrence_id)
+            WHERE
+                c.vocabulary_id LIKE 'ICD%' AND
+                ({code_clause}) AND
+                (co.condition_type_concept_id IN (38000200,38000201,
+                38000202,38000203,38000204,38000205,38000214,
+                38000206,38000207,38000208,38000209,38000210,
+                38000211,38000212,38000213) OR
+                v.visit_concept_id IN (9201,9203))
+            ")
+  }
   result_all <- download_big_data(query,dest)
   result_all <- window_data(result_all,"condition_start_date",anchor_date_table,before,after)
   return(result_all)
