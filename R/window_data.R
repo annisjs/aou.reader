a3b02e78cc4a4ceb98b752780f5eeab53fb989e8
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-05-23 10:51:42 -0500

    Allow cartesian joins in window data

--- R/window_data.R
+++ R/window_data.R
@@ -2,7 +2,7 @@ window_data <- function(dat,date_column,anchor_date_table,before,after)
 {
     if (!is.null(anchor_date_table))
     {
-        dat <- data.table::as.data.table(merge(dat,anchor_date_table,by="person_id"))
+        dat <- data.table::as.data.table(merge(dat,anchor_date_table,by="person_id",allow.cartesian=TRUE))
         dat[,min_window_date := anchor_date + before]
         dat[,max_window_date := anchor_date + after]
         dat <- dat[get(date_column) >= min_window_date]
