3e6534b6a212439b059cbc4f790ce42130a241b9
Cihan <adnancihan.cakar@vumc.org>
2024-09-17 15:00:07 -0400

    updates lab concept query

--- R/lab_concept_query.R
+++ R/lab_concept_query.R
@@ -1,8 +1,6 @@
 #' Lab concept query
 #' 
 #' @param labs a character vector or string containing the labs concepts to query
-#' @param value_as_number a numeric vector to subset labs by values as number
-#' @param value_as_concept_id a numeric vector to subset labs by values as concept ids
 #' @param anchor_date_table a data.frame containing two columns: person_id, anchor_date. A time window can be defined around the anchor date using the \code{before} and \code{after} arguments.
 #' @param before an integer greater than or equal to 0. Dates prior to anchor_date + before will be excluded.
 #' @param after an integer greater than or equal to 0. Dates after anchor_date + after will be excluded.
@@ -16,11 +14,8 @@
 #' lab_concept_dat <- lab_concept_query(c(586520,586523,586525))
 #' }
 #' @export 
-lab_concept_query <- function(lab_concepts, value_as_number=NULL, value_as_concept_id=NULL, anchor_date_table=NULL,before=NULL,after=NULL)
+lab_concept_query <- function(lab_concepts, anchor_date_table=NULL,before=NULL,after=NULL)
 {
-  if(!is.null(value_as_number) & !is.null(value_as_concept_id)){
-    stop("Only value as a number or value as a concept id can be set for subsetting.")
-  }
   dest <- "lab_concept_query_result.csv"
   lab_concepts <- paste(lab_concepts,collapse=', ')
   query <- stringr::str_glue("
@@ -31,10 +26,5 @@ lab_concept_query <- function(lab_concepts, value_as_number=NULL, value_as_conce
         ")
   result_all <- download_big_data(query,dest)
   result_all <- window_data(result_all,"measurement_date",anchor_date_table,before,after)
-  if(!is.null(value_as_number)){
-    result_all = subset(result_all, value_as_number %in% value_as_number & !is.na(value_as_number))
-  } else if(!is.null(value_as_concept_id)){
-    result_all = subset(result_all, value_as_concept_id %in% value_as_concept_id & !is.na(value_as_concept_id))
-  } 
   return(result_all)
 }
