caf7f280c72c9171322e31a616302a88edefe775
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2024-11-05 15:27:08 -0600

    Added drug type column

--- R/outpatient_med_query.R
+++ R/outpatient_med_query.R
@@ -7,7 +7,7 @@
 #' 
 #' @return 
 #' a data.table containing the following columns:
-#' person_id, drug_exposure_start_date
+#' person_id, drug_exposure_start_date, drug_type
 #' @examples
 #' \dontrun{
 #' med_dat <- outpatient_med_query(c("roflumilast","daliresp"))
@@ -18,7 +18,7 @@ outpatient_med_query <- function(meds,anchor_date_table=NULL,before=NULL,after=N
   dest <- "outpatient_med_query_result.csv"
   med_terms <- paste('lower(c.concept_name) LIKE ',"'%",meds,"%'",collapse=' OR ',sep="")
   query <- stringr::str_glue("
-       SELECT DISTINCT d.person_id, d.drug_exposure_start_date
+       SELECT DISTINCT d.person_id, d.drug_exposure_start_date, c2.concept_name AS drug_type
         FROM
         drug_exposure d
         INNER JOIN
@@ -27,6 +27,9 @@ outpatient_med_query <- function(meds,anchor_date_table=NULL,before=NULL,after=N
         INNER JOIN
         visit_occurrence v 
         ON (d.visit_occurrence_id = v.visit_occurrence_id)
+        INNER JOIN
+        concept c2
+        ON (d.drug_type_concept_id = c2.concept_id)
         WHERE
         ({med_terms}) AND
         v.visit_concept_id = 9202
