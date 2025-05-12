8f6a40e1156ad1326a4e56bea4669d7d9ed0a9de
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2025-05-12 07:41:04 -0500

    missing person_id

--- R/wear_study_query.R
+++ R/wear_study_query.R
@@ -19,6 +19,7 @@ wear_study_query <- function(anchor_date_table=NULL,before=NULL,after=NULL)
     dest <- "wear_study_query.csv"
     query <- paste("
             SELECT
+               person_id,
                resultsconsent_wear AS wear_study_consent,
                wear_consent_start_date AS wear_study_consent_start_date,
                wear_consent_end_date AS wear_study_consent_end_date
