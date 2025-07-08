50bc6b05c93ac384a88a83d3f660cfe166cc3e65
cakarac <adnancihan.cakar@vumc.org>
2025-07-08 11:41:54 -0500

    updates medical encounter query

--- R/medical_encounter_query.R
+++ R/medical_encounter_query.R
@@ -32,7 +32,8 @@ medical_encounter_query <- function(time="first",anchor_date_table=NULL,before=N
     stop("time must be 'first', 'last', 'count' or 'all'")
   }
   dest <- stringr::str_glue("{time}_medical_encounter_query_result.csv")
-  query <- stringr::str_glue("
+  if (time == "first" || time == "last"){
+    query <- stringr::str_glue("
     WITH ehr AS (
     SELECT person_id, {ordering}(m.measurement_date) AS date
     FROM `measurement` AS m
@@ -71,11 +72,51 @@ medical_encounter_query <- function(time="first",anchor_date_table=NULL,before=N
     GROUP BY person_id
     )
 
-    SELECT person_id, {ordering}(date) as {time}_medical_encounter_entry_date
+    SELECT person_id, {ordering}(date) as medical_encounter_entry_date
     FROM ehr
-    GROUP BY person_id {date_group}
-    ")
+    GROUP BY person_id
+    ")} else{
+      query <- stringr::str_glue("
+            WITH ehr AS (
+            SELECT DISTINCT person_id, m.measurement_date AS date
+            FROM `measurement` AS m
+            LEFT JOIN `measurement_ext` AS mm on m.measurement_id = mm.measurement_id
+            WHERE LOWER(mm.src_id) LIKE 'ehr site%'
+
+            UNION DISTINCT
+
+            SELECT DISTINCT person_id, m.condition_start_date AS date
+            FROM `condition_occurrence` AS m
+            LEFT JOIN `condition_occurrence_ext` AS mm on m.condition_occurrence_id = mm.condition_occurrence_id
+            WHERE LOWER(mm.src_id) LIKE 'ehr site%'
+            
+            UNION DISTINCT
+
+            SELECT DISTINCT person_id, m.procedure_date AS date
+            FROM `procedure_occurrence` AS m
+            LEFT JOIN `procedure_occurrence_ext` AS mm on m.procedure_occurrence_id = mm.procedure_occurrence_id
+            WHERE LOWER(mm.src_id) LIKE 'ehr site%'
+
+            UNION DISTINCT
+
+            SELECT DISTINCT person_id, m.visit_end_date AS date
+            FROM `visit_occurrence` AS m
+            LEFT JOIN `visit_occurrence_ext` AS mm on m.visit_occurrence_id = mm.visit_occurrence_id
+            WHERE LOWER(mm.src_id) LIKE 'ehr site%'
+            
+            UNION DISTINCT
+
+            SELECT DISTINCT person_id, m.drug_exposure_start_date AS date
+            FROM `drug_exposure` AS m
+            )
+            SELECT person_id, date as medical_encounter_entry_date
+            FROM ehr
+            ")
+    }
   result_all <- download_big_data(query,dest)
+  if (time == "count"){
+    result_all = result_all[, .(count_of_medical_encounter = .N), .(person_id)]
+  }
   result_all <- window_data(result_all,"medical_encounter_entry_date",anchor_date_table,before,after)
   return(result_all)
 }
