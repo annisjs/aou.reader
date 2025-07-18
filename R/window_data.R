a0810105802881df9010ff20fae3661bb66b7e03
Jeffrey Annis <jeffrey.s.annis@vumc.org>
2025-07-18 15:20:33 -0500

    Setting before/after to default when anchor date is provided

--- R/window_data.R
+++ R/window_data.R
@@ -1,15 +1,15 @@
 window_data <- function(dat,date_column,anchor_date_table,before,after)
 {
-    if (is.null(before))
-    {
-        before <- -100000
-    }
-    if (is.null(after))
-    {
-        after <- 100000
-    }
     if (!is.null(anchor_date_table))
     {
+        if (is.null(before))
+        {
+            before <- -100000
+        }
+        if (is.null(after))
+        {
+            after <- 100000
+        }
         dat <- data.table::as.data.table(merge(dat,anchor_date_table,by="person_id",allow.cartesian=TRUE))
         dat[,min_window_date := anchor_date + before]
         dat[,max_window_date := anchor_date + after]
